216aa1c40718f16a4e8f5c0aea06b805492ab542
c0s13 <80195826+c0s13@users.noreply.github.com>
2022-03-03 16:30:40 +0400

    Update speck_k64_p32_o32_r22.py
    Adding check_test_vector() function

--- avalanche_data_generator/speck_32_64/speck_k64_p32_o32_r22.py
+++ avalanche_data_generator/speck_32_64/speck_k64_p32_o32_r22.py
@@ -599,3 +599,20 @@ def generate_avalanche_dataset(num_samples):
           data[j][:, i*32:(i+1)*32]=result[j]^base[j]
       diff=np.roll(diff, -1, axis=0)
   return data
+
+def check_test_vector():
+    key = [0x19, 0x18, 0x11, 0x10, 0x09, 0x08, 0x01, 0x00]
+    pt = [0x65, 0x74, 0x69, 0x4c]
+    ct = [0xa8, 0x68, 0x42, 0xf2]
+
+    key = np.unpackbits(np.array(key, dtype=np.uint8))
+    pt = np.unpackbits(np.array(pt, dtype=np.uint8))
+    ct = np.unpackbits(np.array(ct, dtype=np.uint8))
+
+    result = evaluate([key.reshape((64, 1)), pt.reshape((32, 1))]) 
+    if ((result[21][0] == ct).all()):
+        print("Test Vector Verified")
+        return True
+    else:
+        print("Test Vector NOT Verified")
+        return False
