1668b7c0dd150445b463ceef2c8a2b1960a5b26d
cs <yunju.huang@tii.ae>
2022-03-06 01:19:35 +0400

    revise generateData.py

--- avalanche_data_generator/speck_64_128/generateData.py
+++ avalanche_data_generator/speck_64_128/generateData.py
@@ -12,7 +12,7 @@ from datetime import timedelta
 t = time.time()
 dataset=data_generator.generate_avalanche_dataset(number_of_samples)
 for r in range(len(dataset)):
-    np.save("dataset_speck_64_128_"+"_rounds_"+str(r)+'_'+str(number_of_samples)+"_samples.npy",dataset[r])
+    np.save("dataset_speck_64_128_"+"_round_"+str(r)+'_'+str(number_of_samples)+"_samples.npy",dataset[r])
 t = time.time() - t
 f_out = open("execution_time.txt", "a")
 f_out.write(str(timedelta(seconds=t))+"\n")
