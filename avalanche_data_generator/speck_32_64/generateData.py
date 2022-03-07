561cd5fca12e6abc42e56077bac30369e64c7f1c
cs <yunju.huang@tii.ae>
2022-03-07 15:46:27 +0400

    fixed bugs

--- avalanche_data_generator/speck_32_64/generateData.py
+++ avalanche_data_generator/speck_32_64/generateData.py
@@ -12,7 +12,7 @@ from datetime import timedelta
 t = time.time()
 dataset=data_generator.generate_avalanche_dataset(number_of_samples)
 for r in range(len(dataset)):
-    np.savez_compressed("dataset_speck_32_64_"+"_round_"+str(r)+'_'+str(number_of_samples)+"_samples.npz",dataset=dataset[r])
+    np.savez_compressed("dataset_speck_32_64_"+"_round_"+str(r)+'_'+str(number_of_samples)+"_samples",dataset=dataset[r])
 t = time.time() - t
 f_out = open("execution_time.txt", "a")
 f_out.write(str(timedelta(seconds=t))+"\n")
