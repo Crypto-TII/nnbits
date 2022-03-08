6c53374c7f7bbd9c6782caa267d25e1c795a293b
cs <yunju.huang@tii.ae>
2022-03-08 11:30:57 +0400

    revise generateData.py to avoid killed signal

--- avalanche_data_generator/speck_128_128/generateData.py
+++ avalanche_data_generator/speck_128_128/generateData.py
@@ -10,9 +10,9 @@ import time
 from datetime import timedelta
 
 loops = 100
-for i in range(50):
+for i in range(loops):
     t = time.time()
-    dataset=data_generator.generate_avalanche_dataset(int(number_of_samples/50))
+    dataset=data_generator.generate_avalanche_dataset(int(number_of_samples/loops))
     for r in range(len(dataset)):
         np.savez_compressed("dataset_speck_128_128_"+"_round_"+str(r)+'_'+str(number_of_samples)+"_samples_"+str(i),dataset=dataset[r])
     t = time.time() - t
